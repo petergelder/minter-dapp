a39b62e7f1fdcfb495a432597b53b9dfa6ba8c63
Peter Gelder <peter.gelder@capital6games.co.uk>
2022-06-24 16:42:45 +0100

    fsde

--- frontend/js/wallet.js
+++ frontend/js/wallet.js
@@ -1,23 +1,26 @@
-let web3;
 let accounts;
 
 async function Connect(){
     if (window.ethereum) {
-        web3 = new Web3(window.ethereum);
-    } else if (window.web3) {
-        web3 = new Web3(window.web3.currentProvider);
-    }
-
-    if (web3) {
-    // Check if User is already connected by retrieving the accounts
-    await window.web3.eth.getAccounts().then(async (addr) => {
-        accounts = addr;
-    });
-    }
-
-    if (MetaMaskOnboarding.isMetaMaskInstalled()) {
+        window.web3 = new Web3(window.ethereum);
+        checkChain();
+      } else if (window.web3) {
+        window.web3 = new Web3(window.web3.currentProvider);
+      }
+    
+      if (window.web3) {
+        // Check if User is already connected by retrieving the accounts
+        await window.web3.eth.getAccounts().then(async (addr) => {
+          accounts = addr;
+        });
+      }
+    
+      updateConnectStatus();
+    
+      if (MetaMaskOnboarding.isMetaMaskInstalled()) {
         window.ethereum.on("accountsChanged", (newAccounts) => {
           accounts = newAccounts;
+          updateConnectStatus();
         });
-    }
+      }
 }
