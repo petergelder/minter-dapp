1f0d538571cbab781630d930a8ffd9d25ce560ef
Peter Gelder <peter.gelder@capital6games.co.uk>
2022-06-26 20:15:04 +0100

    regd

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -18,7 +18,7 @@ const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT";
+const button_public_mint = "Mint NFT";
 const mint_failed = "Minting failed. 😢 Please try again.";
 
 const abi = [
