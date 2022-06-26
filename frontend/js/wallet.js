7bf4a7e7815091d8cf09b7d0ee8abb8d37c4ad70
Peter Gelder <peter.gelder@capital6games.co.uk>
2022-06-26 14:23:50 +0100

    gf

--- frontend/js/wallet.js
+++ frontend/js/wallet.js
@@ -7,18 +7,20 @@ window.addEventListener("DOMContentLoaded", async () => {
     if (window.ethereum) {
         window.web3 = new Web3(window.ethereum);
         checkChain();
-      } else if (window.web3) {
+    } else if (window.web3) {
         window.web3 = new Web3(window.web3.currentProvider);
-      }
+    }
 
-      if (window.web3) {
-        // Check if User is already connected by retrieving the accounts
-        await window.web3.eth.getAccounts().then(async (addr) => {
-          accounts = addr;
-          await Connect();
-        });
-      }
-    });
+    if (window.web3) {
+    // Check if User is already connected by retrieving the accounts
+    await window.web3.eth.getAccounts().then(async (addr) => {
+        accounts = addr;
+        
+      });
+    }
+    
+    await Connect();
+});
 
 async function loadInfo() {
 
